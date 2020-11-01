b916cf51b19307aae504e1ebf46e5526a4e88581
harshhx <harshgupta07042001@gmail.com>
2020-11-02 01:18:09 +0530

    working prediction function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import warnings
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("white")
 
 # ignore warnings #
 warnings.filterwarnings("ignore")
@@ -13,5 +17,26 @@ movie_titles = pd.read_csv("ml-100k/u.item", sep="\|", header=None)
 movie_titles = movie_titles[[0, 1]]
 movie_titles.columns = ["item_id", "title"]
 
-df = pd.merge(df,movie_titles,on="item_id")
-print(df.shape)
+df = pd.merge(df, movie_titles, on="item_id")
+
+ratings = pd.DataFrame(df.groupby("title").mean()["rating"])
+ratings["number of ratings"] = pd.DataFrame(df.groupby("title").count()["rating"])
+
+movieMat = df.pivot_table(index="user_id", columns="title", values="rating")
+
+
+# prediction Function #
+
+def predict_movie(movie):
+    movie_user_rating = movieMat[movie]
+    similar_to_movie = movieMat.corrwith(movie_user_rating)
+
+    corr_movie = pd.DataFrame(similar_to_movie, columns=["correlation"])
+    corr_movie.dropna(inplace=True)
+    corr_movie = corr_movie.join(ratings["number of ratings"])
+    predictions = corr_movie[corr_movie["number of ratings"] > 100].sort_values("correlation", ascending=False)
+
+    return predictions
+
+predictions = predict_movie("Titanic (1997)")
+print(predictions.head())
